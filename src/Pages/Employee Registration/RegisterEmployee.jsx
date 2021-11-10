3d844e283cf06a26e82a73062ab5184c99a0b48f
Laxman Rai <himitsu.fushigi@protonmail.com>
2021-11-10 17:41:00 +0545

    Working on register form

--- src/Pages/Employee Registration/RegisterEmployee.jsx
+++ src/Pages/Employee Registration/RegisterEmployee.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import "./style.scss";
 
 import Webcam from "react-webcam";
+import { Button } from "@mui/material";
 
 import { getFullFaceDescription, loadModels } from "../../API/face";
+
+import FacialImg from "../../assets/images/facial.jpg";
 
 const videoConstraints = {
   width: 1280,
@@ -13,6 +16,22 @@ const videoConstraints = {
 
 export default function RegisterEmployee() {
   const webcamRef = React.useRef(null);
+  const [cameraOpened, setCameraOpened] = React.useState(false);
+  const [employeeImage, setEmployeeImage] = React.useState({
+    captured: false,
+    image: null,
+  });
+  const [employeeImageDescriptor, setEmployeeImageDescriptor] = React.useState(
+    []
+  );
+
+  /**
+   * @dev form elements
+   */
+  const [fullName, setFullName] = React.useState("");
+  const [dob, setDob] = React.useState(Date.now());
+  const [contactNumber, setContactNumber] = React.useState("");
+  const [joinedAt, setJoinedAt] = React.useState(Date.now());
 
   React.useEffect(() => {
     (async () => {
@@ -21,35 +40,95 @@ export default function RegisterEmployee() {
   }, []);
 
   const capture = React.useCallback(() => {
+    setEmployeeImage({
+      captured: true,
+      image: webcamRef.current.getScreenshot(),
+    });
+
     (async () => {
       if (!!webcamRef.current) {
         await getFullFaceDescription(
           webcamRef.current.getScreenshot(),
           160
         ).then((fullDesc) => {
-            /**
-             * * extract descriptors, format then store to db
-             */
-          if (!!fullDesc) console.log(fullDesc);
+          /**
+           * * extract descriptors, format then store to db
+           */
+          if (!!fullDesc) setEmployeeImageDescriptor(fullDesc[0].descriptor);
         });
       }
     })();
   }, [webcamRef]);
 
+  const handleCameraOpen = () => {
+    setCameraOpened(true);
+  };
+
+  const handleRegisterEmployee = () => {
+    console.log(employeeImageDescriptor);
+  };
+
   return (
     <div className="registerEmployee__container">
       <h1>Hash Technologies</h1>
-      <Webcam
-        audio={false}
-        height={400}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-        width={400}
-        videoConstraints={videoConstraints}
-        style={{ objectFit: "cover" }}
-        mirrored={true}
-      />
-      <button onClick={capture}>Capture photo</button>
+
+      {/**
+       * * Check if camera is opened or not
+       */}
+      {!!cameraOpened ? (
+        <div className="registerEmployee__container__cameraOpened">
+          {!employeeImage.captured ? (
+            <div className="registerEmployee__container__cameraOpened__camera">
+              <Webcam
+                audio={false}
+                height={250}
+                ref={webcamRef}
+                screenshotFormat="image/jpeg"
+                width={250}
+                videoConstraints={videoConstraints}
+                style={{ objectFit: "cover" }}
+                mirrored={true}
+              />
+              <Button onClick={capture}>Capture photo</Button>
+            </div>
+          ) : (
+            <div className="registerEmployee__container__cameraOpened__camera">
+              <img src={employeeImage.image} alt="employee" />
+              <Button
+                variant="contained"
+                onClick={() =>
+                  setEmployeeImage({ captured: false, image: null })
+                }
+              >
+                Take Again
+              </Button>
+            </div>
+          )}
+
+          <form>
+            <label htmlFor="">Full Name</label>
+            <input type="text" placeholder="Himitsu Fushigi" />
+
+            <label htmlFor="">Date of Birth</label>
+            <input type="date" />
+
+            <label htmlFor="">Contact Number</label>
+            <input type="text" placeholder="9812345678" />
+
+            <label htmlFor="">Joined At</label>
+            <input type="date" />
+
+            <Button onClick={handleRegisterEmployee} variant="contained">
+              Register Employee
+            </Button>
+          </form>
+        </div>
+      ) : (
+        <div className="registerEmployee__container__cameraNotOpened">
+          <img src={FacialImg} />
+          <Button onClick={handleCameraOpen}>Open Camera</Button>
+        </div>
+      )}
     </div>
   );
 }
